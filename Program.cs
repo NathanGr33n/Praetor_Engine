413236c10b5781f85f8ee6994cf387b37773820a
nathan.green.223@gmail.com <nathan.green.223@gmail.com>
2025-07-16 20:08:28 -0700

    Initial Project Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Praetor_Engine
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run;
+        }
+    }
+}
